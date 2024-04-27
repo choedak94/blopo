d00ea69afd0fc9bfd0883381fc76592c36a46752
tenzin <tenzin@nine.ch>
2024-04-27 19:05:03 +0200

    add pagination

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,6 +4,7 @@ class BlogPostsController < ApplicationController
 
 	def index
 		@blog_posts = user_signed_in? ? BlogPost.all : BlogPost.published
+		@pagy, @blog_posts = pagy(@blog_posts)
 	end
 
 	def show
