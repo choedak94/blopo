55287b0a19a0efd1102556b6c5e211f1adc0c2d3
choedak94 <79316173+choedak94@users.noreply.github.com>
2025-07-03 12:39:02 +0200

    Add video upload and playback

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -49,7 +49,7 @@ class BlogPostsController < ApplicationController
   private
 
   def resource_params
-    params.require(:blog_post).permit(:title, :content, :published_at)
+    params.require(:blog_post).permit(:title, :content, :published_at, :video)
   end
 
   def load_blog_post
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,7 @@
 class BlogPost < ApplicationRecord
-	belongs_to :user
+        belongs_to :user
   has_rich_text :content
+  has_one_attached :video
 
 	validates :title, presence: true
 	validates :content, presence: true
