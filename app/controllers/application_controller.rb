e64a8b047d9b965ff73ce2771142bb0516d0def5
choedak94 <79316173+choedak94@users.noreply.github.com>
2025-07-08 12:00:24 +0200

    Add bilingual support with locale selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-	include Pagy::Backend
-	before_action :configure_permitted_parameters, if: :devise_controller?
+        include Pagy::Backend
+        before_action :set_locale
+        before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected
 
-	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-		devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-	end
+        def configure_permitted_parameters
+                devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+                devise_parameter_sanitizer.permit(:account_update, keys: [:username, :language])
+        end
+
+        def set_locale
+                I18n.locale = current_user&.language || I18n.default_locale
+        end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :blog_posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+
+  validates :language, inclusion: { in: %w[en zh] }, presence: true
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
